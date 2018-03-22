3b8eabae4d752602e1b5c0ae8c8a37b2a23d03e1
Ykat <ykat@Yekaterinas-MacBook-Pro.local>
2018-03-21 18:33:53 -0700

    react shell

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">EduRide</h1>
+          <p className="App-intro">
+        Schedule and book rides for your children from the people you know and trust - fellow parents, guardians, and members of your school community. 
+        </p>
+        </header>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
